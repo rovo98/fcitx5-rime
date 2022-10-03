0050addc332fbfe7dd9528ea7997c7e077fd5b0a
dcLunatic <dcLunatic6@gmail.com>
2022-10-03 12:57:36 +0800

    support some flypyly function

--- src/rimestate.cpp
+++ src/rimestate.cpp
@@ -255,6 +255,8 @@ void RimeState::updateUI(InputContext *ic, bool keyRelease) {
     bool oldEmptyExceptAux = emptyExceptAux(inputPanel);
     engine_->updateAction(ic);
 
+    std::string allPreedit("");
+
     do {
         auto api = engine_->api();
         if (!api || api->is_maintenance_mode()) {
@@ -270,6 +272,10 @@ void RimeState::updateUI(InputContext *ic, bool keyRelease) {
             break;
         }
 
+        if (context.composition.preedit){
+            allPreedit += context.composition.preedit;
+        }
+        
         updatePreedit(ic, context);
 
         if (context.menu.num_candidates) {
@@ -297,6 +303,15 @@ void RimeState::updateUI(InputContext *ic, bool keyRelease) {
         ic->updateUserInterface(UserInterfaceComponent::StatusArea);
     }
 
+    if(engine_->config().flypyHideInputPannel.value() && 
+            allPreedit.find("`") == std::string::npos &&
+            allPreedit.find("ob") != 0 &&
+            allPreedit.find("of") != 0 &&
+            (allPreedit.find("ot") != 0 || allPreedit.length() <= 2) &&
+            allPreedit.find("ox") != 0
+            ) {
+        inputPanel.reset();
+    }
     if (!keyRelease || !oldEmptyExceptAux || !newEmptyExceptAux) {
         ic->updateUserInterface(UserInterfaceComponent::InputPanel);
     }
